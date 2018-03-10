91da80a4e959368458d895bdd287238c4a11f582
Ari Madian <akmadian@gmail.com>
2018-03-10 00:22:17 -0800

    Added groundwork for api authentication, downloading albums from imgur, and parsing submissions.
     - Downloading albums uses code by github user alexgisby
        - Code does not support downloading individual images
        - Code possibly supports downloading galleries
        - Code does not support, as far as I can tell with about half an hour of working
           with it, saving images to a subdirectory.

---
+++ BattlestationsBot.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+    File Name: BattlestationsBot.py
+    Author: Ari Madian
+    Created: March 9, 2018 11:13 PM
+    Python Version: 3.6
+    Repo: github.com/akmadian/Machine-Learning
+    Twitter: twitter.com/BattlestatBot
+"""
+
+import config_secret
+import tweepy
+import praw
+from imguralbum import ImgurAlbumDownloader
+
+
+class BotInstance:
+
+    def __init__(self):
+        self.redditinstance = self.reddit_authenticate()
+        self.twitterinstance = self.twitter_authenticate()
+
+    @staticmethod
+    def reddit_authenticate():
+        print('Authenticating Reddit...')
+        reddit = praw.Reddit(client_id=config_secret.client_id,
+                     client_secret=config_secret.client_secret,
+                     password=config_secret.password,
+                     user_agent=config_secret.user_agent,
+                     username=config_secret.username)
+        print('Successfully Authenticated Reddit...')
+        return reddit
+
+    @staticmethod
+    def twitter_authenticate():
+        print('Authenticating Twitter...')
+        auth = tweepy.OAuthHandler(config_secret.c_key,
+                                   config_secret.c_secret)
+        auth.set_access_token(config_secret.a_token,
+                              config_secret.a_secret)
+        api = tweepy.API(auth)
+        print('Successfully Authenticated Twitter...')
+        return api
+
+    @staticmethod
+    def download_imgur_album(album_url):
+        downloader = ImgurAlbumDownloader(album_url)
+        downloader.save_images()
+
+    def parse_new_submissions(self):
+        subreddit = self.redditinstance.subreddit('battlestations')
+        for submission in subreddit.hot(limit=10):
+            print(submission.title)
+            print(submission.url)
+            if submission.url.split('/')[3] == 'a':
+                self.download_imgur_album(submission.url)
+            elif submission.url.split('/')[3] == 'gallery':
+                self.download_imgur_album(submission.url)
+
+botinstance = BotInstance()
+botinstance.parse_new_submissions()
+'''
+subreddit_ = botinstance.redditinstance.subreddit('battlestations')
+print(subreddit_)
+for submission in subreddit_.hot(limit=10):
+    print(submission.title)
+    print(submission.url.split('/'))
+    # botinstance.download_imgur_album(submission.url)
+'''
---
+++ config_public.py
@@ -0,0 +1,12 @@
+# REDDIT API AUTH
+client_id     = ''
+client_secret = ''
+password      = ''
+username      = ''
+user_agent    = ''
+
+# TWITTER API AUTH
+c_key     = ''
+c_secret  = ''
+a_token   = ''
+a_secret  = ''
